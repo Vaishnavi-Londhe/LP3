23d8cc4d0716e0a356006bee5066ed2fd506674b
Vaishnavi-Londhe <160232692+Vaishnavi-Londhe@users.noreply.github.com>
2025-11-14 21:50:18 +0530

    Update NQueens.java

--- NQueens.java
+++ NQueens.java
@@ -1,89 +1,138 @@
 import java.util.*;
 
 public class NQueens {
-    int n;
-    int[][] board;
 
-    NQueens(int n) {
-        this.n = n;
-        board = new int[n][n];
+    int size;
+    boolean[][] board;
+    int count = 0;
+    int fixedRow = -1, fixedCol = -1;
+
+    // Constructor
+    NQueens() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of chessboard: ");
+        size = sc.nextInt();
+
+        board = new boolean[size][size];
     }
 
+    // Print board
+    void printBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(board[i][j] ? "Q " : "X ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    // Check if safe to place queen
     boolean isSafe(int row, int col) {
-        // Check row on left
-        for (int i = 0; i < col; i++)
-            if (board[row][i] == 1)
+
+        // Check column
+        for (int i = 0; i < size; i++)
+            if (board[i][col])
                 return false;
 
-        // Upper-left diagonal
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
-            if (board[i][j] == 1)
-                return false;
+        // Back slash (\) diagonal upper
+        int i = row, j = col;
+        while (i >= 0 && j >= 0) {
+            if (board[i][j]) return false;
+            i--;
+            j--;
+        }
 
-        // Lower-left diagonal
-        for (int i = row, j = col; j >= 0 && i < n; i++, j--)
-            if (board[i][j] == 1)
-                return false;
+        // Back slash (\) diagonal lower
+        i = row; j = col;
+        while (i < size && j < size) {
+            if (board[i][j]) return false;
+            i++;
+            j++;
+        }
+
+        // Forward slash (/) diagonal upper
+        i = row; j = col;
+        while (i >= 0 && j < size) {
+            if (board[i][j]) return false;
+            i--;
+            j++;
+        }
+
+        // Forward slash (/) diagonal lower
+        i = row; j = col;
+        while (i < size && j >= 0) {
+            if (board[i][j]) return false;
+            i++;
+            j--;
+        }
 
         return true;
     }
 
-    boolean solve(int col) {
-        if (col == n)    // all queens placed
-            return true;
-
-        for (int row = 0; row < n; row++) {
-
-            if (board[row][col] == 1) {   // fixed queen already here
-                if (isSafe(row, col))
-                    return solve(col + 1);
-                else
-                    return false;
-            }
-
-            if (isSafe(row, col)) {
-                board[row][col] = 1;   // place queen
-
-                if (solve(col + 1))
-                    return true;
-
-                board[row][col] = 0;   // backtrack
-            }
-        }
-        return false;
-    }
-
-    void printBoard() {
-        for (int[] row : board) {
-            for (int cell : row)
-                System.out.print(cell == 1 ? "Q " : "X ");
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args) {
+    // Set the first queen manually
+    void setFirstQueen() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter n: ");
-        int n = sc.nextInt();
+        System.out.println("Enter coordinates of first queen:");
+        System.out.print("Enter row (1-" + size + "): ");
+        fixedRow = sc.nextInt() - 1;
 
-        NQueens obj = new NQueens(n);
+        System.out.print("Enter column (1-" + size + "): ");
+        fixedCol = sc.nextInt() - 1;
 
-        System.out.print("Enter fixed Queen row (1-" + n + "): ");
-        int r = sc.nextInt();
+        board[fixedRow][fixedCol] = true;
+        printBoard();
+    }
 
-        System.out.print("Enter fixed Queen column (1-" + n + "): ");
-        int c = sc.nextInt();
+    // Backtracking function
+    void solve(int row) {
 
-        // Place fixed queen
-        obj.board[r - 1][c - 1] = 1;
-
-        // Start solving from column 0
-        if (obj.solve(0)) {
-            System.out.println("\nSolution:");
-            obj.printBoard();
-        } else {
-            System.out.println("No solution possible with this fixed Queen.");
+        if (row == size) {
+            count++;
+            printBoard();
+            return;
         }
+
+        // If fixed queen is in this row → skip placing and move to next row
+        boolean rowHasQueen = false;
+        for (int j = 0; j < size; j++) {
+            if (board[row][j]) {
+                rowHasQueen = true;
+                break;
+            }
+        }
+
+        if (rowHasQueen) {
+            solve(row + 1);
+            return;
+        }
+
+        // Try all columns
+        for (int col = 0; col < size; col++) {
+            if (isSafe(row, col)) {
+                board[row][col] = true;
+
+                solve(row + 1);
+
+                board[row][col] = false; // backtrack
+            }
+        }
+    }
+
+    // Final message
+    void displayMessage() {
+        if (count > 0)
+            System.out.println("Solution exists for the given position of the queen.");
+        else
+            System.out.println("Solution doesn't exist for the given position of the queen.");
+    }
+
+    // Main
+    public static void main(String[] args) {
+        NQueens solver = new NQueens();
+        solver.setFirstQueen();
+        solver.solve(0);
+        solver.displayMessage();
     }
 }
